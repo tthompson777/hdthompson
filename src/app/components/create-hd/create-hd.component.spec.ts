0219be8c5c8ec4cb88f92cbd7712d49ffd133a81
Tiago Thompson <tiago.thompson@deliverymuch.com.br>
2023-03-31 23:04:06 -0300

    Revert "v11"
    This reverts commit 01f361c9a05776fe85e1959f49f5439521dec74e.

--- src/app/components/create-hd/create-hd.component.spec.ts
+++ src/app/components/create-hd/create-hd.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateHdComponent } from './create-hd.component';
 
@@ -6,7 +6,7 @@ describe('CreateHdComponent', () => {
   let component: CreateHdComponent;
   let fixture: ComponentFixture<CreateHdComponent>;
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CreateHdComponent ]
     })
